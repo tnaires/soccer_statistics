9b6652cc7ce4f4f1d4b78fc3fddb369acea8ad87
Tarso Aires <tnaires@gmail.com>
2016-04-27 01:43:02 -0300

    Configure Swagger

--- src/main/java/com/uninassau/soccer/Application.java
+++ src/main/java/com/uninassau/soccer/Application.java
@@ -1,12 +1,42 @@
 package com.uninassau.soccer;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
 		application.run(args);
 	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(Predicates.not(regex("/error.*")))
+                .build();
+	}
+
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Soccer Statistics")
+                .build();
+    }
 }
--- src/main/java/com/uninassau/soccer/controller/Index.java
+++ src/main/java/com/uninassau/soccer/controller/Index.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.uninassau.soccer.model.Team;
 import com.uninassau.soccer.repository.Teams;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class Index {
 	private final Teams teams;
@@ -17,6 +21,10 @@ public class Index {
 		this.teams = teams;
 	}
 	
+	@ApiOperation(value = "Get all soccer statistics")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Statistics for all the teams")
+	})
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public Iterable<Team> getSoccerStatistics() {
 		return teams.findAll();
